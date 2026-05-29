8e72c31c0542b0b57032bc833a465e7c753c711f
Ivan <ivanibarra1@icloud.com>
2026-05-28 21:52:15 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // Big O(m*n) is bilinear time comp 
+        // execution time depends on product of two indep. input vars 
+        HashMap<String, List<String>> hashmap = new HashMap<>();
+        for (String s: strs) {
+            // 1. convert string to char array and sort it
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+
+            // 2. create the uniform key
+            String key = new String(chars);
+
+            // 3. put the key in map (initialize if it doesnt exist)
+            hashmap.putIfAbsent(key, new ArrayList<>());
+            hashmap.get(key).add(s);
+        }
+        return new ArrayList<>(hashmap.values());
+    }
+}
